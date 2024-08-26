bff4f7d030beafe202a9bfaf5bb04a10098c9980
KirillDemyanenko <demyanenkokg@gmail.com>
2024-08-26 10:39:34 +0300

    feat: task 8

---
+++ task8/src/main.rs
@@ -0,0 +1,20 @@
+/*
+    Напишите программу, которая считывает два целых числа a (i8) и b (i8) и
+    выводит для них операцию побитового ИЛИ в виде сообщений.   
+*/
+
+fn main() {
+    let mut user_input = String::new();
+    std::io::stdin().read_line(&mut user_input).expect("Input error!");
+    let a: i8 = user_input
+        .trim()
+        .parse()
+        .expect("Par/se error!");
+    user_input.clear();
+    std::io::stdin().read_line(&mut user_input).expect("Input error!"); 
+    let b: i8 = user_input
+        .trim()
+        .parse()
+        .expect("Parse error!");
+    println!("{a:08b} :a\n{b:08b} :b\n{:08b} :a | b", a | b)
+}
