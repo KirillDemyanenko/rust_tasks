d404c6b9f15e320a338bb7cc342d581a8fa8be70
KirillDemyanenko <demyanenkokg@gmail.com>
2024-09-04 13:48:27 +0300

    refactor: create completed folder

--- task1/target/debug/deps/task1-40c6e66e32c9a88c.d
+++
@@ -1,5 +0,0 @@
-F:\Study\rust\rust_tasks\task1\target\debug\deps\libtask1-40c6e66e32c9a88c.rmeta: src/main.rs
-
-F:\Study\rust\rust_tasks\task1\target\debug\deps\task1-40c6e66e32c9a88c.d: src/main.rs
-
-src/main.rs:
--- task1/target/debug/deps/task1-9d6e8edb6b65b8fe.d
+++
@@ -1,5 +0,0 @@
-F:\Study\rust\rust_tasks\task1\target\debug\deps\libtask1-9d6e8edb6b65b8fe.rmeta: src/main.rs
-
-F:\Study\rust\rust_tasks\task1\target\debug\deps\task1-9d6e8edb6b65b8fe.d: src/main.rs
-
-src/main.rs:
