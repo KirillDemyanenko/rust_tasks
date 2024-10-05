6edf0aa87e90558908f52f2d1ff40cfaf7a1e0df
KirillDemyanenko <demyanenkokg@gmail.com>
2024-10-05 14:29:15 +0300

    feat: task 107

---
+++ completed/task107/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("i j");
+    for i in 0..3 {
+        for j in 0..3 {
+            for k in 0..3 {
+                println!("{i} {j} {k}");
+            }
+        }
+    }
+}
