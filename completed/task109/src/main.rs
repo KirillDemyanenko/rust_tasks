02972013b35fef4fe89f5261f3b7327d9556e3c1
KirillDemyanenko <demyanenkokg@gmail.com>
2024-10-07 14:12:47 +0300

    feat: task 109

---
+++ completed/task109/src/main.rs
@@ -0,0 +1,27 @@
+/*
+    Напишите программу, которая считывает натуральное число n (u8) и выводит треугольник из * с высотой равным n.
+*/
+use std::fmt::Debug;
+use std::str::FromStr;
+
+fn input<T: FromStr>() -> T
+where
+    <T as FromStr>::Err: Debug,
+{
+    let mut buffer: String = String::new();
+    std::io::stdin()
+        .read_line(&mut buffer)
+        .expect("Input error!");
+    buffer.trim().parse::<T>().expect("Parse error")
+}
+
+fn main() {
+    let n = input::<u8>();
+    let mut spaces = (n - 1) as i8;
+    let mut stars = 1;
+    for _ in 0..n {
+        println!("{}{}", " ".repeat(spaces as usize), "*".repeat(stars as usize));
+        stars += 2;;
+        spaces -= 1;
+    }
+}
